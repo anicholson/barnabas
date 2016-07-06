6ddc1066fc792b4e07f35735a0cde782b93feab4
Andy Nicholson <andrew@anicholson.net>
2016-07-06 22:09:37 +1000

    Replace accidental binary Applescript with plaintext version (transparency FTW!)

--- src/message_sender.cr
+++ src/message_sender.cr
@@ -1,7 +1,7 @@
 require "./recipient"
 
 class MessageSender
-  SENDER_SCRIPT = "barnabas.scpt"
+  SENDER_SCRIPT = "barnabas.applescript"
 
   def self.send(recipient : Recipient, message : String) : Bool
     script_path = File.join(File.dirname(__FILE__), ".", SENDER_SCRIPT)
