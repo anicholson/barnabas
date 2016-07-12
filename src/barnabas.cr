bf77eaf8dd4f326f40ce837451cef5a1d6e3af28
Andy Nicholson <andrew@anicholson.net>
2016-07-12 17:00:58 +1000

    Don't crash and burn when odd options get passed

--- src/barnabas.cr
+++ src/barnabas.cr
@@ -7,17 +7,26 @@ message = ""
 
 recipients = RecipientRepository.new(ENV["HOME"] + "/.barnabas/contacts.toml")
 
-parser = OptionParser.parse! do |parser|
-  parser.on("-m MESSAGE", "Message to send") {|msg| message = msg }
+parser = OptionParser.new
+parser.on("-m MESSAGE", "Message to send") {|msg| message = msg }
 
-  parser.on("-t NAME", "--to=NAME", "Name of the contact to message") do |name|
-    if recipient = recipients.fetch(name)
-      contact = recipient
-    end
+parser.on("-t NAME", "--to=NAME", "Name of the contact to message") do |name|
+  if recipient = recipients.fetch(name)
+    contact = recipient
   end
-
-  parser.on("-h", "--help", "Show this help") { puts parser; exit 0 }
 end
+
+parser.on("-h", "--help", "Show this help") { puts parser; exit 0 }
+
+begin
+  parser.parse!
+rescue e : OptionParser::Exception
+  puts e.message
+  puts parser
+
+  exit 1
+end
+
 
 if contact == Recipient::BLANK
   puts "You need to specify a recipient!"
