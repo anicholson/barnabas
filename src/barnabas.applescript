c0ab84702d6404b27339b472b91300ab4ea1c8bb
Andy Nicholson <andrew@anicholson.net>
2016-07-12 15:44:36 +1000

    Inline the applescript to reduce the number of moving parts in an installed version

--- src/barnabas.applescript
+++
@@ -1,8 +0,0 @@
-on run argv
-	set contact to item 1 of argv
-	set message to item 2 of argv
-	
-	tell application "Messages"
-		send message to buddy contact of (service 1 whose service type is iMessage)
-	end tell
-end run
--- src/message_sender.cr
+++ src/message_sender.cr
@@ -1,12 +1,27 @@
 require "./recipient"
 
 class MessageSender
-  SENDER_SCRIPT = "barnabas.applescript"
+  SENDER_SCRIPT = <<-APPLESCRIPT
+on run argv
+  set contact to item 1 of argv
+  set message to item 2 of argv
+
+  tell application "Messages"
+    send message to buddy contact of (service 1 whose service type is iMessage)
+  end tell
+end run
+
+APPLESCRIPT
 
   def self.send(recipient : Recipient, message : String) : Bool
-    script_path = File.join(File.dirname(__FILE__), ".", SENDER_SCRIPT)
     puts "Sending message to #{recipient.name}"
-    process = Process.run("osascript", [script_path, "#{recipient.contact}", "#{message}"], nil, false, false, nil, true)
+    process = Process.run("osascript", ["-", "#{recipient.contact}", "#{message}"], nil, false, false, nil, true) do |p|
+      input = p.input
+
+      input << SENDER_SCRIPT
+
+      p.wait
+    end
 
     process.success?
   end
