6eb6cd19ac7bd91fd42562d743cb578cfdd84645
Andy Nicholson <andrew@anicholson.net>
2016-09-18 16:23:12 +1000

    First go at a suggestion script

---
+++ script/suggest.zsh
@@ -0,0 +1,12 @@
+#! /usr/bin/env zsh
+
+function _barnabas_suggest {
+    ((we_suggest_chance = 6))
+    ((suggest = $RANDOM % $we_suggest_chance))
+
+    [[ 0 -eq $suggest ]] && $(barnabas suggest)
+
+}
+
+
+_barnabas_suggest
