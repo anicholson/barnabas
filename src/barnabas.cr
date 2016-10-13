947efc25cbf88488bb8d74594527da8098ba4d17
Andy Nicholson <andrew@anicholson.net>
2016-10-14 07:46:53 +1100

    Don't crash when an unknown command is passed
    (Fixes #4)

--- src/barnabas.cr
+++ src/barnabas.cr
@@ -7,12 +7,28 @@ require "./command"
 
 recipients = RecipientRepository.new(ENV["BARNABAS_HOME"] + "/" + ENV["BARNABAS_DB"])
 
+def exit_with_code(result : Symbol)
+  known_codes = {
+    :OK => 0,
+    :FAIL => 1
+  }
+
+  exit_code = begin
+                known_codes[result]
+              rescue KeyError
+                known_codes[:FAIL]
+              end
+
+  exit(exit_code)
+end
+
 def command_to_run
   command = (ARGV.any? && ARGV.shift) || "help"
 end
 
 command = Command::Repository[command_to_run]
 
-command.process(ARGV, recipients)
+result = command.process(ARGV, recipients)
 
-exit(1)
+
+exit_with_code(result)
